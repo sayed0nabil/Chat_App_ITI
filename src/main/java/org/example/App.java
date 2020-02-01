72d9872e63125f9c67a63ec18518039ddb0ab9ac
Abdulrahman Hamed <abdelrahmanh.habashi@gmail.com>
2020-02-01 18:06:45 +0200

    App title

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
+        stage.setTitle("Chat Application");
         stage.show();
     }
 
