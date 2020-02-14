5d9f6ff3ec59ef7fb746abe0a200e6f9f49c4967
sayed0nabil <sayed0nabil@gmail.com>
2020-02-14 09:30:13 +0200

    Change IChatService Methods with new Signture

--- src/main/java/org/example/Interfaces/IChatService.java
+++ src/main/java/org/example/Interfaces/IChatService.java
@@ -11,12 +11,12 @@ import java.util.List;
 
 public interface IChatService extends Remote {
 
-    List<User> getFriendList(int userId) throws RemoteException;
+    List<User> getFriendList(User user) throws RemoteException;
     void changeStatus(UserStatus userStatus) throws RemoteException;
-    Message sendMessage(User user) throws RemoteException;
+    Message sendMessage(int userId, Message message) throws RemoteException;
     List<Notification>getNotifications() throws RemoteException;
-    void register(User user) throws RemoteException;
-    void unRegister(User user) throws RemoteException;
+    void register(int userId, IUserService userService) throws RemoteException;
+    void unRegister(int userId) throws RemoteException;
     void addFriend() throws RemoteException;
     void sendGroupMsg(User user, Message groupMessage) throws RemoteException;
 }
--- src/main/java/org/example/Interfaces/IUserService.java
+++ src/main/java/org/example/Interfaces/IUserService.java
@@ -1,6 +1,10 @@
 package org.example.Interfaces;
 
+import org.example.model.Message;
+
 import java.rmi.Remote;
 
 public interface IUserService extends Remote {
+
+    void receiveMessage(Message message);
 }
